f9f385728699d929d5a9232e0f859fae08706ba2
Nazareth villalba <nazarethv99@gmail.com>
2024-10-18 00:23:42 +0200

    Initial Commit

---
+++ ZaraRick&Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZaraRick&Morty
+//
+//  Created by Nazareth villalba on 18/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
